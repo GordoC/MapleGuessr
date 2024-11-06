8537837e9a0b46e6e0656b78e3010b62b33741e8
GordoC <gordonch99@gmail.com>
2024-11-05 18:38:51 -0800

    logout user

--- client/src/Components/Navbar/Navbar.tsx
+++ client/src/Components/Navbar/Navbar.tsx
@@ -1,16 +1,27 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.scss';
+import { useAuth } from "../../Context/useAuth";
 
 const Navbar = () => {
+    const { isLoggedIn, user, logoutUser } = useAuth();
     return (
         <nav className="navbar">
             <div className="navbar-title">
                 <h1>MapleGuessr</h1>
             </div>
-            <div className="navbar-links">
-                <Link to="/login" className="navbar-link">Login</Link>
-                <Link to="/register" className="navbar-link">Register</Link>
+            <div >
+                { isLoggedIn() ? (
+                    <div className="navbar-links">
+                        <div>Welcome, {user?.userName}</div>
+                        <div onClick={logoutUser}>Logout</div>
+                    </div>
+                ) : (
+                    <div className="navbar-links">
+                        <Link to="/login" className="navbar-link">Login</Link>
+                        <Link to="/register" className="navbar-link">Signup</Link>
+                    </div>
+                )}
             </div>
         </nav>
     );
